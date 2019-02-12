c73f6f7dee22cd4322673a187bce3ca02523ba62
Bryan Vargas <BVargas949@west-mec.org>
2019-02-12 16:08:14 -0700

    Final Commit : run clock enabled
    Certain values were replaced as new variables were created, the function cock was created to make the clock count down on the web page.

--- tny_script.js
+++ tny_script.js
@@ -7,47 +7,42 @@
 
    Countdown Clock
    Author: Bryan Vargas
-   Date:   02/05/19
+   Date:   02/05/18
 
 */
-// window.alert("Welcome to Tulsa");
 
-/* display time and date */
+/* Execute the function to run and display the countdown clock */
+runClock();
+setInterval("runClock()", 1000);
 
-document.getElementById("dateNow").innerHTML = "m/d/y<br />h:ms";
+/* Function to create and run the countdown clock */
+function runClock() {
+    /* Store the current date and time */
+    var currentDay = new Date();
+    var dateStr = currentDay.toLocaleDateString();
+    var timeStr = currentDay.toLocaleTimeString();
 
-// displayed time left till New Years Eve
+    /* Display the current date and time */
+    document.getElementById("dateNow").innerHTML =
+        dateStr + "<br />" + timeStr;
 
-document.getElementById("days").textContent = "dd";
-document.getElementById("hrs").textContent = "hh";
-document.getElementById("mins").textContent = "mm";
-document.getElementById("secs").textContent = "ss";
+    /* Calculate the days until January 1st */
+    var newYear = new Date("January 1, 2018");
+    var nextYear = currentDay.getFullYear() + 1;
+    newYear.setFullYear(nextYear);
+    var daysLeft = (newYear - currentDay) / (1000 * 60 * 60 * 24);
 
-//Store the Current date and Time
-var currentDay = new Date("May 23, 2018 14:35:05");
-var dateStr = currentDay.toLocaleDateString();
-var timeStr = currentDay.toLocaleTimeString();
+    /* Calculate the hours left in the current day */
+    var hrsLeft = (daysLeft - Math.floor(daysLeft)) * 24;
 
-//Display the current Time and Date
-document.getElementById("dateNow").innerHTML = dateStr + "<br />" + timeStr;
+    /* Calculate the minutes and seconds left in the current hour */
+    var minsLeft = (hrsLeft - Math.floor(hrsLeft)) * 60;
+    var secsLeft = (minsLeft - Math.floor(minsLeft)) * 60;
 
-//Calculate the dates and time
-var newYear = new Date("January 1, 2018");
-var nextYear = currentDay.getFullYear() + 1;
-newYear.setFullYear(nextYear);
-var daysleft = (newYear - currentDay) / (1000 * 60 * 60 * 24);
 
-//display time until new years
-document.getElementById("days").textContent = daysleft;
-
-document.getElementById("days").textContent = Math.floor(daysleft);
-
-/* Calculate the hours left in the current day */
-var hrsLeft = (daysLeft - Math.floor(daysLeft)) * 24;
-var minsLeft = (hrsLeft - Math.floor(hrsLeft)) * 60;
-var secLeft = (minsLeft - Math.floor(minsLeft)) *
-
-    document.getElementById("days").textContent = Math.floor(daysleft);
-document.getElementById("hrs").textContent = Math.floor(hrsLeft);
-document.getElementById("mins").textContent = "mm";
-document.getElementById("secs").textContent = "ss";
+    /* Display the time left until New Year's Eve */
+    document.getElementById("days").textContent = Math.floor(daysLeft);
+    document.getElementById("hrs").textContent = Math.floor(hrsLeft);
+    document.getElementById("mins").textContent = Math.floor(minsLeft);
+    document.getElementById("secs").textContent = Math.floor(secsLeft);
+}
